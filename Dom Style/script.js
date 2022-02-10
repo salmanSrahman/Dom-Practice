5937ccfa8700007c36fb246e10f099e354cdf69b
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-10 12:02:02 +0600

    dom style

---
+++ Dom Style/script.js
@@ -0,0 +1,20 @@
+const firstTitle = document.getElementById("first-title");
+firstTitle.style.color = "red";
+firstTitle.style.fontSize = "30px";
+
+// ====== title selected by id & applied style
+
+const secondTitle = document.getElementById("second-title");
+secondTitle.style.color = "blue";
+
+// ====== title selected by id & applied style
+
+const thirdTitle = document.getElementById("third-title");
+thirdTitle.style.color = "green";
+
+// ====== title selected by id & applied style
+
+const lastTitle = document.getElementById("last-title");
+lastTitle.style.color = "goldenrod";
+
+// ====== title selected by id & applied style
