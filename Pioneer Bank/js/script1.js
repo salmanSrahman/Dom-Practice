6ab6c53ed5450d48a08032bdbf8f25f116f6f5dd
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-13 15:20:02 +0600

    transaction part added

--- Pioneer Bank/js/script1.js
+++ Pioneer Bank/js/script1.js
@@ -6,4 +6,20 @@ document.getElementById("login-btn").addEventListener("click", function () {
   const userPassword = document.getElementById("user-password");
   const userPasswordValue = userPassword.value;
   console.log(userPasswordValue);
+  //   ====== get user password ======
+
+  if (userEmailValue == "") {
+    document.getElementById("message").innerHTML = "**Fill the email please!";
+    return false;
+  }
+  if (userPasswordValue == "") {
+    document.getElementById("message").innerHTML =
+      "**Fill the password please!";
+    return false;
+  }
+  var secretEmail = "pioneer@gmail.com";
+  var secretNumber = 123456;
+  if (userEmailValue == secretEmail && userPasswordValue == secretNumber) {
+    window.location.href = "banking.html";
+  }
 });
