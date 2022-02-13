8dd31d860badf2684d8408eb0447005692da3256
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-13 19:39:01 +0600

    deposit part end

--- Pioneer Bank/js/banking.js
+++ Pioneer Bank/js/banking.js
@@ -1,5 +1,12 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
   const depositInput = document.getElementById("deposit-input");
-  const depositAmount = depositInput.value;
-  console.log(depositAmount);
+  const depositValue = parseFloat(depositInput.value);
+  console.log(depositValue);
+
+  const depositAmount = document.getElementById("deposit-amount");
+  const depositAmountText = parseFloat(depositAmount.innerText);
+
+  const depositTotal = depositAmountText + depositValue;
+  depositAmount.innerText = depositTotal;
+  console.log(depositTotal);
 });
