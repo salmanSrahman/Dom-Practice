631373f2024e0c0ee3857f1771b818a478e2de22
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-14 23:33:28 +0600

    functional bank

---
+++ functional bank/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  const userEmail = document.getElementById("user-email");
+  const userEmailValue = userEmail.value;
+  console.log(userEmailValue);
+  //   ====== get user email ======
+});
+
+// ======= log in section to transaction part =========
