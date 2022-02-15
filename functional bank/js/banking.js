353f78638500628e5ea543f7e3425bf26a2384a2
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-15 20:15:14 +0600

    functional bank

---
+++ functional bank/js/banking.js
@@ -0,0 +1,19 @@
+const balanceAmount = document.getElementById("balance-amount");
+const balanceAmountText = parseFloat(balanceAmount.innerText);
+
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const depositInput = document.getElementById("deposit-input");
+  const depositValue = parseFloat(depositInput.value);
+  const depositAmount = document.getElementById("deposit-amount");
+  const depositAmountText = parseFloat(depositAmount.innerText);
+
+  const depositTotal = depositAmountText + depositValue;
+  depositAmount.innerText = depositTotal;
+
+  const balanceTotal = balanceAmountText + depositValue;
+  balanceAmount.innerText = balanceTotal;
+
+  depositInput.value = "";
+});
+
+// ========== deposit & balance part end ============
