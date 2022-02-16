270cdcec27a7119714dd47e39a47e1e4c04eba79
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-16 19:53:07 +0600

    functional bank

--- functional bank/js/banking.js
+++ functional bank/js/banking.js
@@ -1,47 +1,8 @@
-const balanceAmount = document.getElementById("balance-amount");
-const balanceAmountText = parseFloat(balanceAmount.innerText);
+const depositBtn = document.getElementById("deposit_btn");
 
-function getInput(id) {
-  const depositInput = document.getElementById(id);
-  const depositValue = parseFloat(depositInput.value);
-  depositInput.value = "";
-  return depositValue;
-}
-
-function updateAmount(id) {
+function inputField(id) {
   const depositAmount = document.getElementById(id);
-  const depositAmountText = parseFloat(depositAmount.innerText);
-  const depositTotal = depositAmountText + depositValue;
-  depositAmount.innerText = depositTotal;
+  const depositAmountValue = document.getElementById(id).value;
+  const depositAmountValueNumber = parseFloat(depositAmountValue);
+  return depositAmountValueNumber;
 }
-
-document.getElementById("deposit-btn").addEventListener("click", function () {
-  const depositValue = getInput("deposit-input");
-
-  //   const depositAmount = document.getElementById("deposit-amount");
-  //   const depositAmountText = parseFloat(depositAmount.innerText);
-  //   const depositTotal = depositAmountText + depositValue;
-  //   depositAmount.innerText = depositTotal;
-  updateAmount("deposit-amount");
-
-  const balanceTotal = balanceAmountText + depositValue;
-  balanceAmount.innerText = balanceTotal;
-});
-
-// ========== deposit & balance part end ============
-document.getElementById("withdraw-btn").addEventListener("click", function () {
-  const withdrawValue = getInput("withdraw-input");
-
-  const withdrawAmount = document.getElementById("withdraw-amount");
-  const withdrawAmountText = parseFloat(withdrawAmount.innerText);
-  const withdrawTotal = withdrawValue + withdrawAmountText;
-  withdrawAmount.innerText = withdrawTotal;
-
-  const balanceTotal = balanceAmountText - withdrawTotal;
-  balanceAmount.innerText = balanceTotal;
-});
-
-// ============ withdraw & balance part end ===========
-document.getElementById("complete-btn").addEventListener("click", function () {
-  window.location.href = "complete.html";
-});
